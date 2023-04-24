5e6709f43a28a52d7888ddb75026e09944cc286d
Gabriel Gamboa <gcarlos.gabriel4@gmail.com>
2023-04-24 09:33:48 -0400

    multiples ajustes producto de un mal merge
    multiples ajustes producto de un mal merge

--- assets/js/digimon.js
+++ assets/js/digimon.js
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-
-function idDigimonRandom(){ // genera un id aleatorio para ver los datos de un digimon aleatorio
-    const primerDigimon = 1; //primer id digimon
-    const ultimoDigimon = 1422; //ultimo id digimon
-    const id = Math.floor(Math.random() * (ultimoDigimon - primerDigimon +1)) + primerDigimon;
-    //console.log("id random generado");
-    return id;
-}
-
-function cardDigimonRandom(digimon){ //genera el card para ser renderizado en el DOM
-    const card =`
-        <div id="digimon-${digimon.numero}" class="card digimon scale-in-center" data-toggle="modal" data-target="#digiModal">
-            <img class="card-img-top rounded-circle" style="max-height: 50%;" src='${digimon.imagen}' alt="digimon ${digimon.numero}">
-            <div class="card-body">
-                    <h4 class="card-subtitle mb-2 text-muted">#${digimon.numero}</h4>
-                    <h3 class="card-title">${digimon.nombre}</h3>
-                    <input type="hidden" value='${digimon.tipos}'>
-                    <input type="hidden" value='${digimon.descripcion}'>
-            </div>
-=======
 // VARIABLES Y CONSTANTES
 const primerDgm = 1;    //primer digimon 1
 const ultimoDgm = 1422; //ultimo digimon 1422
@@ -51,30 +30,12 @@ function generarCard(digimon){
             <p hidden class="card-niveles">${digimon.niveles}</p>
             <p hidden class="card-lanzamiento">${digimon.lanzamiento}</p>
             <p hidden class="card-descripcion">${digimon.descripcion}</p>
->>>>>>> Stashed changes
         </div>
     `;
     //console.log("card generado");
     return card;
 }
 
-<<<<<<< Updated upstream
-function obtenerDigimon(id){ // busqueda de digimon random
-    const url= `https://digimon-api.com/api/v1/digimon/${id}`;
-    //console.log('url api digimon...');
-    return fetch(url)
-    .then(response => response.json() )
-    .then(data => {
-        const digimon = {
-            numero : data.id,
-            nombre : data.name,
-            imagen : data.images[0].href,
-            tipos : data.types,
-            descripcion: data.descriptions
-        }
-        // almacenar solo la descripcion en ingles 
-        let textoIngles;
-=======
 function prueba(){
     alert('esto es una pruebirijilla')
 }
@@ -97,32 +58,12 @@ function obtenerDigimon(id){
         };
         //capturar descripcion en ingles
         let textoIngles=''; 
->>>>>>> Stashed changes
         digimon.descripcion.forEach(descripcion => {
             if (descripcion.language === "en_us"){
                 textoIngles = descripcion.description
             }
         })
         digimon.descripcion = textoIngles;
-<<<<<<< Updated upstream
-        // almacenar los type name en un solo string, ya que vienen todos en un array
-        let tipos = digimon.tipos.map(tipo => tipo.type).join(", ");
-        digimon.tipos = tipos;
-        //console.log("digimon generado");
-        return digimon;
-    })
-    .catch(error => console.error(error))
-}
-
-async function quienEsEseDigimon(){
-    const idRandom = idDigimonRandom();
-    const digimon = await obtenerDigimon(idRandom);
-    if(digimon){
-        const dgmCard= cardDigimonRandom(digimon);
-        const digimonContenedor = document.querySelector("#random-digimon");
-        digimonContenedor.insertAdjacentHTML('beforeend', dgmCard);
-        return console.log(digimon.nombre + ' agregado');    }
-=======
 
         //capturar los tipos del digimon
         let tipos = digimon.tipos.map(tipo => tipo.type).join(", ");
@@ -152,16 +93,8 @@ async function quienEsEseDigimon(){
     let digimon = await obtenerDigimon(id);
     //console.table(digimon)
         renderizarDigimones(digimon);
->>>>>>> Stashed changes
 }
 // MODAL DIGIMON
-<<<<<<< Updated upstream
-function limpiarModal(){
-    $('.modal-numero').text('');
-    $('.modal-nombre').text('');
-    $('.modal-tipo').text('');
-    $('.modal-descripcion').text('');
-=======
 
 function mostrarModal(elementoId){
     //limpio el modal de los datos anteriore
@@ -216,7 +149,6 @@ function limpiarModal(){
     descrModal.text('');
     imgModal.attr('src', '');
     // innecesario imgModal.removeClass("bounce-in-top")
->>>>>>> Stashed changes
 }
 
 
